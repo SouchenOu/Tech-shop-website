cf1ebbe016ffbf6b2200148330ae4fb8f42b19c8
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2024-01-22 18:32:59 +0100

    impliment html lines by using javascript

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -3,7 +3,7 @@ console.log("yes");
 const products = [
     
     {
-        image : "images/products/athletic-cotton-socks-6-pairs.jpg",
+        image : "images/products/1.jpeg",
         name : "Black and Gray Athletic Cotton Socks - 6 Pairs",
         price : "$10.90",
         rating : {
@@ -12,7 +12,7 @@ const products = [
         }
     },
     {
-        image : "images/products/intermediate-composite-basketball.jpg",
+        image : "images/products/2.jpeg",
         name : "Intermediate Size Basketball",
         price : "$20.95",
         rating : {
@@ -22,7 +22,32 @@ const products = [
         }
     },
     {
-        image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        image : "images/products/3.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    },
+    {
+        image : "images/products/4.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    }, {
+        image : "images/products/5.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    }, {
+        image : "images/products/6.jpeg",
         name : "Adults Plain Cotton T-Shirt - 2 Pack",
         price : "$7.99",
         rating : {
