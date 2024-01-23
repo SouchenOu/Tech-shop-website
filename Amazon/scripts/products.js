92a84cb64455371e7e48f3f44bbc2aaf7ba41ae6
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2024-01-23 13:13:37 +0100

    create another file product.js to put all the product their

---
+++ Amazon/scripts/products.js
@@ -0,0 +1,56 @@
+const products = [
+    
+    {
+        image : "images/products/1.jpeg",
+        name : "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        price : "$10.90",
+        rating : {
+            stars : 4.5,
+            count : 87,
+        }
+    },
+    {
+        image : "images/products/2.jpeg",
+        name : "Intermediate Size Basketball",
+        price : "$20.95",
+        rating : {
+            stars: 3,
+            count: 127,
+
+        }
+    },
+    {
+        image : "images/products/3.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    },
+    {
+        image : "images/products/4.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    }, {
+        image : "images/products/5.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    }, {
+        image : "images/products/6.jpeg",
+        name : "Adults Plain Cotton T-Shirt - 2 Pack",
+        price : "$7.99",
+        rating : {
+            stars : 4.5,
+            count : 56,
+        }
+    }
+]
